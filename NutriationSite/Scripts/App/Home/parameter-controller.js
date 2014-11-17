83190bc55be8b34a64a313b208298d78c0549641
Igor <igor.mzg@gmail.com>
2014-11-17 03:21:52 +0200

    Add new Application

---
+++ NutriationSite/Scripts/App/Home/parameter-controller.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    $('#addParameterBtn')
+      .button()
+      .click(function (e) {
+          $('#dialog-add-parameter').dialog({
+              resizable: false,
+              modal: true,
+              width: 340,
+              buttons: {
+                  Add: function () {
+                      $('#dialog-add-parameter form').submit();
+                      $(this).dialog('close');
+                  },
+                  Cancel: function () {
+                      $(this).dialog('close');
+                  }
+              }
+          });
+      });
+
+    $('.addParameterValueBtn')
+      .button()
+      .click(function (e) {
+          var id = $(this).data('parameter-id');
+          $('#dialog-add-value form input[name=ParameterId]').val(id);
+          $('#dialog-add-value').dialog({
+              resizable: false,
+              modal: true,
+              width: 340,
+              buttons: {
+                  Add: function () {
+                      $('#dialog-add-value form').submit();
+                      $(this).dialog('close');
+                  },
+                  Cancel: function () {
+                      $(this).dialog('close');
+                  }
+              }
+          });
+      });
+
+    $.getJSON("Home/GetProductChartData", drawCharts);
+});
+
+function drawCharts(data) {
+    console.log(data, 'data in draw charts');
+    $('#parameters > ul').append('<li>')
+        .append('<div id="chart-container">')
+        .append('<input class="addParameterValueBtn" type="button" value="Add value" data-parameter-id="@item.ParameterId" />');
+}
