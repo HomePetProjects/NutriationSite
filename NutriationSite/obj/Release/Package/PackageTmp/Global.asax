a2faa7daf62e10c8af6c9076794be4249f84d4d9
igormzg <igor.mzg@gmail.com>
2015-01-19 18:21:54 +0200

    Last commit

---
+++ NutriationSite/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NutriationSite.MvcApplication" Language="C#" %>
---
+++ NutriationSite/obj/Release/Package/PackageTmp/Scripts/App/Home/Layout.js
@@ -0,0 +1,16 @@
+﻿//Document ready
+$(document).ready(function () {
+    window.PN.chart.init();
+});
+
+//Namespace class
+window.PN.layout = {
+
+
+    init: function () {
+    },
+
+    currentPage: function (){
+
+    }
+}
